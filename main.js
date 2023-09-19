b65a8d5938c07c5b07cfb14d377c03b173796d26
Your Name <vilasicoding@gmail.com>
2023-09-19 14:20:00 -0400

    Initial Commit

---
+++ main.js
@@ -0,0 +1,26 @@
+// main.js
+const { app, BrowserWindow } = require('electron');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  mainWindow.loadFile('index.html');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', function () {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
---
+++ renderer.js
@@ -0,0 +1,3 @@
+// renderer.js
+// Put your client-side JavaScript here
+console.log('Renderer process started');
